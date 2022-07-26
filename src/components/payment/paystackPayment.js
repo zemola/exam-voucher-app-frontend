85198e7d15cab1d96f6ec39fceadb4ea48e5410e
unknown <oluwadamilolasandraodujirin@gmai.com>
2022-07-26 15:41:46 +0100

    payment

--- src/components/payment/paystackPayment.js
+++ src/components/payment/paystackPayment.js
@@ -1,8 +1,16 @@
 import PaystackPop from '@paystack/inline-js'
 import { useState } from 'react';
+import './payment.css'
+
+import { toast } from 'react-toastify';
+
 
 
 const PaystackPayment = ({price}) => {
+
+  const notify = () => toast ("Successful Add to Cart!");
+
+
   const [email, setEmail] = useState('');
   // const [amount, setAmount] = useState(price);
   const [firstName, setFirstName] = useState('');
@@ -22,6 +30,7 @@ const PaystackPayment = ({price}) => {
       onSuccess(transaction) {
         let message = `payment Complete Reference ${transaction.refrence}`
         console.log(message)
+        notify(); 
       },
       onCancel() {
         console.log("transaction cancled")
@@ -33,30 +42,38 @@ const PaystackPayment = ({price}) => {
 
   }
 
-  return ( <div
-  ><h1>PayStack</h1>
+  return ( 
+  <div className='payment'>
+    <div className='sub-payment' > 
       <div>
-        <form onSubmit={payWithPaystack}>
-          <div>
-            <label>Email Address</label>
-            <input type="email" id='email-address' onChange={(e)=>setEmail(e.target.value)} />
+        <h1 className='payment-text' >PayStack</h1>
+          <div className='form-payment'>
+            <form onSubmit={payWithPaystack}>
+              <div>
+                <label className='payment-form'>Email</label>
+                <input type="email" id='email-address' onChange={(e)=>setEmail(e.target.value)} />
+              </div>
+              <div>
+                <label className='payment-form'>Amount</label>
+                <input type="tel" id='amount' value={`N ${price}`} disabled />
+              </div>
+              <div>
+                <label className='payment-form'>First Name</label>
+                <input type="text" id='first-name' onChange={(e)=>setFirstName(e.target.value)} />
+              </div>
+              <div>
+                <label className='payment-form'>Last Name</label>
+                <input type="text" id='last-name' onChange={(e)=>setLastName(e.target.value)} />
+              </div>
+              <div>
+                <button className='btn'>Pay</button>
+              </div>
+            </form>
           </div>
-          <div>
-            <label>Amount</label>
-            <input type="tel" id='amount' value={`N ${price}`} disabled />
-          </div>
-          <div>
-            <label>First Name</label>
-            <input type="text" id='first-name' onChange={(e)=>setFirstName(e.target.value)} />
-          </div>
-          <div>
-            <label>Last Name</label>
-            <input type="text" id='last-name' onChange={(e)=>setLastName(e.target.value)} />
-          </div>
-          <div><button>Pay</button></div>
-        </form>
       </div>
-  </div> );
+    </div>  
+  </div> 
+  );
 }
  
 export default PaystackPayment;
